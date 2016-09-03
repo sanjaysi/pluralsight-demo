563520cc5f13affcb5583ec5e56c68aa73b6d3e1
unknown <Sanjay.Singh@ATMECSINLT-011.ATMECS.CORP>
2016-09-03 19:54:41 +0530

    Done: Initial App Structure.

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class AboutPage extends Component {
+    render() {
+        return(
+            <div>
+                <h1>About Page</h1>
+                <p>This application is a administrator app for 
+                    PluralSight authors</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Admin 2.0</h1>
+                <p>React, Redux, React Router, Babel, Webpack, 
+                    npm scripts, ES6, etc. for building ultra
+                    responsive web app.</p>
+                <Link to="about">Learn more</Link>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
